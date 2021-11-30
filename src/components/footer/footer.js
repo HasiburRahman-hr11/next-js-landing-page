b5cee47476d97b2cd8da62b4529f1455dd1b8367
hrhasib <mailtohrhasib@gmail.com>
2021-11-30 18:22:27 +0600

    added footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,30 @@ import FooterLogo from 'assets/logo.svg';
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <footer sx={styles.footer}>
+      <Container>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link path="/">
+            <Image src={FooterLogo} alt="Logo" />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map((item, i) => (
+                <Link
+                  path={item.path}
+                  key={i}
+                  sx={styles.footer.link}
+                  label={item.label}
+                />
+              ))}
+            </nav>
+          </Box>
+          <Text sx={styles.footer.copyright}>
+                Copyright &copy; {new Date().getFullYear()} Company Name
+          </Text>
+        </Box>
+      </Container>
+    </footer>
   );
 }
 
